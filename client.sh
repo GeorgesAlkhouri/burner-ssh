58fa392786ffa64dfa21bd40566bd63a86796373
Georges Alkhouri <georges.alkhouri@gmail.com>
2021-06-20 11:23:16 +0200

    feat: init commit

---
+++ client.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+set -ue
+
+function _check_deps {
+    tor -h >/dev/null || _err_exit "Couldnt' detect tor" _burn
+    sshd --help 2>&1 | grep -qs OpenSSH || _err_exit "Couldn't detect OpenSSH daemon" _burn
+}
+
+function _random_name {
+    # print 16 - 32bit random string
+    local name
+    name=$(python -c "import secrets; print(secrets.token_hex(16 + secrets.randbelow(17)))")
+    echo "$name"
+}
+
+function _boostrap {
+    _log_cmd mkdir -p "$APP_FOLDER"
+    _burn "$CLIENT_FOLDER"
+    _log_cmd mkdir -p "$CLIENT_FOLDER"
+}
+
+function _print_key {
+    local key
+    key=$(cat "$CLIENT_FOLDER/$1.pub") || _err_exit "Something went wrong no public key" _burn
+    printf "Content of your Public Key File\n\n%s\n\n" \
+           "Give this Key (over a secure channel) to your Server\n" \
+           "and wait for onion service address." "$key"
+}
+
+function _connect {
+    # TODO what about tails
+    ssh -i "$CLIENT_FOLDER/$1" -F "$CLIENT_FOLDER/config" -o IdentitiesOnly=yes \
+        -o "ProxyCommand nc -X 5 -x 127.0.0.1:9050 %h %p" "$2" -vvv \
+        || _err_exit "ssh connection failed" _burn
+}
+
+function _ask_connect {
+    echo "Do you want to connect to the server?"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) _connect "$1" "$2" && break;;
+            No ) _err_exit "User cancel" _burn;;
+        esac
+    done
+}
+
+function _ask_onion {
+    # TODO regex onion address
+    local address
+    while true; do
+        read -rp "Enter onion service address:" address
+        break;
+    done
+    echo "$address"
+}
+
+source ./util.sh
+
+_chec
+k_deps
+_boostrap
+key=$(_create_keys "$CLIENT_FOLDER")
+_print_key "$key"
+# _ask_connect
+address=$(_ask_onion)
+_connect "$key" "$address"
+
---
+++ util.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+set -ue
+
+APP_FOLDER=~/.onionssh
+CLIENT_FOLDER=$APP_FOLDER/client
+SERVER_FOLDER=$APP_FOLDER/server
+
+
+function _create_keys {
+    # Generate ED25519 key pair to folder
+    # $1 - Path to folder
+    #TODO dont safe if folder is not empty
+    local name
+    name=$(_random_name)
+    ssh-keygen -N "" -C "" -t ed25519 -f "$1/$name" >&2
+    echo "$name"
+}
+
+function _burn {
+    # Delete key folders if no path is given
+    # $1 - Path to folder to be deleted
+
+    #TODO add secure deleting
+    if [ "$#" -gt 0 ]; then
+        echo "Burning key folder ($1)"
+        _log_cmd rm -rf "$1"
+    else
+        echo "Burning key folders ($CLIENT_FOLDER, $SERVER_FOLDER)"
+        _log_cmd rm -rf $CLIENT_FOLDER
+        _log_cmd rm -rf $SERVER_FOLDER
+    fi
+}
+
+function _stderr {
+    echo "${*}" 2>&1
+}
+
+function _log_cmd {
+    _stderr "$ ${*}"
+    eval "${*}"
+}
+
+function _err {
+    _stderr "Error: $*"
+}
+
+function _err_exit {
+
+    if [ "$#" -gt 1 ]; then
+        _err "$1"
+        $2
+    fi
+
+    if [ "$#" -eq 1 ]; then
+        _err "$1"
+    fi
+
+    if [ "$#" -eq 0 ]; then
+        _err "Something went wrong."
+    fi
+
+    exit 1
+}
