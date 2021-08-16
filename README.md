6c51b67abbf5ec967e5895859f1dcc1e9ac9a327
Georges Alkhouri <georges.alkhouri@gmail.com>
2021-08-16 18:57:54 +0200

    feat: test hidden service output

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 
 **Project in the making**
 
-Keypoint is to create an anonym admin ssh connection to a server (through the tor network) every time you want to connect.
-- Create an onion service with a custom bootstrapped SSH server behind it. 
-- Connect to the server with one-time keys from a client and burn every down when closing the connection.
+Key-point is to create an anonymous admin SSH connection to a server (through the Tor network) every time you want to connect.
+1. Create an onion service (DONE) with a custom bootstrapped SSH server behind it.
+2. Connect to the server with one-time keys from an SSH client and burn every down when closing the connection.
 
-TODO: write down idea
 
--- src/onionssh/tests/test_tor.py
+++ src/onionssh/tests/test_tor.py
@@ -1,11 +1,16 @@
+import os
+import shutil
 import time
 from collections import defaultdict
 
 import psutil
+import pytest
 
 from onionssh.config import get_tor_config, set_tor_config_value
 
 from ..tor_mixin import TorMixin
+
+HIDDEN_OUTPUT = os.path.join(os.path.dirname(__file__), "hidden")
 
 
 def test_tor_cmd():
@@ -31,7 +36,7 @@ class Mock(TorMixin):
 
 def _mock_config(key):
     assert key == "TOR"
-    set_tor_config_value("HiddenServiceDir", "test2")
+    set_tor_config_value("HiddenServiceDir", HIDDEN_OUTPUT)
     set_tor_config_value("HiddenServicePort", "80 0.0.0.0:80")
     set_tor_config_value("SOCKSPort", "0")
     set_tor_config_value("ExitPolicy", "reject *:*")
@@ -51,13 +56,33 @@ def test_tor_mixin_build_cmd():
 
 
 def _assert_not_tor():
+
     for proc in psutil.process_iter(["name"]):
-        assert (
-            "tor " not in proc.info["name"].lower()
-        ), f"Tor is already running\n{proc}"
+        # info["name"] only contains process name no start params
+        assert "tor" != proc.info["name"], f"Tor is already running\n{proc}"
 
 
-def test_tor_run_mixin():
+def _assert_tor():
+    res = ["tor" == proc.info["name"] for proc in psutil.process_iter(["name"])]
+    assert any(res), "Tor is not running"
+
+
+def _assert_hidden_output():
+    assert os.path.isdir(HIDDEN_OUTPUT)
+    assert os.path.isfile(os.path.join(HIDDEN_OUTPUT, "hostname"))
+
+
+@pytest.fixture
+def clean_hidden_output():
+    print(HIDDEN_OUTPUT)
+    if os.path.isdir(HIDDEN_OUTPUT):
+        shutil.rmtree(HIDDEN_OUTPUT)
+    yield
+    if os.path.isdir(HIDDEN_OUTPUT):
+        shutil.rmtree(HIDDEN_OUTPUT)
+
+
+def test_tor_run_mixin(clean_hidden_output):
     _assert_not_tor()
     mock = Mock()
     setattr(mock, "get_config", _mock_config)
@@ -69,11 +94,16 @@ def test_tor_run_mixin():
     assert mock._tor_thread.is_alive()
     # let tor bootstrap and run into idle
     mock._tor_thread.join(timeout=5.0)
+    # TODO expect some output at least from tor
+    # assert len(mock.outputs) > 0
+    _assert_tor()
     assert mock._tor_thread.is_alive()
+    print("?")
     mock._tor_thread.stop()
     time.sleep(2.0)
     assert not mock._tor_thread.is_alive()
     _assert_not_tor()
+    _assert_hidden_output()
     # assert len(mock.threads) == 0
     # assert len(mock.outputs) == 0
     # assert not mock._tor_thread
